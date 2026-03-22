35276ceaa1e5fa43a88f27b0249f68d245a01c61
Benjamin Attabra <attabeezy@gmail.com>
2026-03-22 01:16:40 +0000

    part 4

--- pymooc_uhelsinki/tmcdata/mooc-programming-26/part04-02_line/src/line.py
+++ pymooc_uhelsinki/tmcdata/mooc-programming-26/part04-02_line/src/line.py
@@ -1,4 +1,6 @@
 # Write your solution here
+def line(count, string):
+    print(string * count)
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
     line(5, "x")
